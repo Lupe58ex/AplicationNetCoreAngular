479f44a9c2bbac9f18007d317f998ad274139647
unknown <lu.58ex@gmail.com>
2020-03-26 21:42:01 -0500

    first local commit

---
+++ WebApplication1/WebApplication1/Models/netcoreContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApplication1.Models
+{
+    public partial class netcoreContext : DbContext
+    {
+        public netcoreContext()
+        {
+        }
+
+        public netcoreContext(DbContextOptions<netcoreContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Value> TableValues { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //if (!optionsBuilder.IsConfigured)
+            //{
+                //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=.;Database=netcore;Trusted_Connection=True;");
+            //}
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
+
